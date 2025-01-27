b97bb0bf1a4d43d9ff25a8746be8ac2c67a7c86f
apcjgarroco <jgarroco@student.apc.edu.ph>
2025-01-27 22:34:11 +0800

    fix

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -1,7 +1,8 @@
 import { createApp } from 'vue'
+
 import App from './App.vue'
 import FoodItem from './components/FoodItem.vue'
 
 const app = createApp(App)
-createApp(App).mount('#app')
 app.component('food-item', FoodItem)
+app.mount('#app')
