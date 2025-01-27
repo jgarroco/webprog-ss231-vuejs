c5ed6311f07db62577c318e23a369c3df78f1e6e
apcjgarroco <jgarroco@student.apc.edu.ph>
2025-01-27 14:22:20 +0000

    update

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,7 +1,15 @@
-<script></script>
- 
 <template>
- <h1>Hello WEBPROG SS231!</h1>
+ <h1>Hello World!</h1>
 </template>
- 
-<style></style>
+
+<script></script>
+
+<style>
+  #app > div {
+    border: dashed black 1px;
+    display: inline-block;
+    margin: 10px;
+    padding: 10px;
+    background-color: lightgreen;
+  }
+</style>
--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import FoodItem from './components/FoodItem.vue'
 
+const app = createApp(App)
 createApp(App).mount('#app')
+app.component('food-item', FoodItem)
