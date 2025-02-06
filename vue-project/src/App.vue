23923936adb97222ce9b724a573f84a83084edb1
apcjgarroco <jgarroco@student.apc.edu.ph>
2025-02-06 20:06:04 +0800

    Comments!

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,23 +1,13 @@
 <template>
-  <div>
-    <h1>Food</h1>
-    <personal-profile/> <br>
-    <div class="food-container">
-      <food-item></food-item>
-      <food-item2></food-item2>
-      <food-item></food-item>
-      <food-item2></food-item2>
-    </div>
-  </div>
+  <h1>Food</h1>
+  <country/>
+  <food-item/>
+  <food-item/>
+  <food-item/>
+  <comment-form/>
+  <comment/>
+   
+
 </template>
 
-<script> </script>
-
-<style>
-.food-container {
-  display: flex;
-  flex-wrap: wrap; 
-  gap: 10px;
-  justify-content: flex-start; 
-}
-</style>
+<script></script>
