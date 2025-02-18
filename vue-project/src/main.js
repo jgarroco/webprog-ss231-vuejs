fe7b04e93a3866b49dff53f8d6d7fd57ef887e1e
apcjgarroco <jgarroco@student.apc.edu.ph>
2025-02-18 13:39:19 +0000

    FORTNITE

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -1,14 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import FoodItem from './components/FoodItem.vue'
-import Country from './components/Country.vue'
-import CommentForm from './components/CommentForm.vue'
-import Comment from './components/Comment.vue'
+import FoodItem2 from './components/FoodItem2.vue'
+import PersonalProfile from './components/PersonalProfile.vue'
+import RestApi from './components/RestApi.vue'
+
 
 const app = createApp(App)
 app.component('food-item', FoodItem)
-app.component('country', Country)
-app.component('comment-form', CommentForm)
-app.component('comment', Comment)
+app.component('food-item2', FoodItem2)
+app.component('personal-profile', PersonalProfile)
+app.component('rest-api', RestApi)
 app.mount('#app')
-
